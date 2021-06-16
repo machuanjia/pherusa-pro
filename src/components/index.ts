c4bdd7b1fbf76208203162d57f05802dd41ec3d7
DayYoung <machuanjia@laiye.com>
2021-06-16 10:49:38 +0800

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,19 @@
 /*
  * @Author: D.Y
  * @Date: 2021-02-04 10:15:26
- * @LastEditTime: 2021-05-10 11:19:00
+ * @LastEditTime: 2021-06-16 09:58:10
  * @LastEditors: D.Y
  * @FilePath: /pherusa-pro/src/components/index.ts
  * @Description: 组件导出文件
  */
 
-export { default as ErrorBoundary } from './ErrorBoundary'
-export { default as NoFond } from './404'
-// export { default as RouteViewer } from './RouteView'
-export { default as GlobalLoading } from './GlobalLoading'
-export * from './RouteView/RouteGenerator'
-export { default as ContentLayoutComponent } from './ContentLayout'
-export { listMixin } from './ListMixin'
-export { asyncComponent } from './RouteView/AsyncComponent'
-export { default as PartLoading } from './PartLoading'
-export { default as Icon } from './Icon'
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as NoFond } from './404';
+export { default as RouteViewer } from './RouteView';
+export { default as GlobalLoading } from './GlobalLoading';
+export * from './RouteView/RouteGenerator';
+export { default as ContentLayoutComponent } from './ContentLayout';
+export { listMixin } from './ListMixin';
+export { asyncComponent } from './RouteView/AsyncComponent';
+export { default as PartLoading } from './PartLoading';
+export { default as Icon } from './Icon';
--- src/utils/history.ts
+++ src/utils/history.ts
@@ -1,20 +1,20 @@
 /*
  * @Author: D.Y
  * @Date: 2021-04-20 10:06:53
- * @LastEditTime: 2021-04-22 15:02:44
+ * @LastEditTime: 2021-06-16 10:10:27
  * @LastEditors: D.Y
- * @FilePath: /laiye-pro/src/utils/history.ts
+ * @FilePath: /pherusa-pro/src/utils/history.ts
  * @Description:
  */
 
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory } from 'history';
 
-export const browserHistory = createBrowserHistory()
+export const browserHistory = createBrowserHistory();
 
 export const goTo = (path: string) => {
-  browserHistory.push(path)
-}
+  browserHistory.push(path);
+};
 
 export const getRoute = () => {
-  return browserHistory.location.pathname
-}
+  return browserHistory.location.pathname;
+};
