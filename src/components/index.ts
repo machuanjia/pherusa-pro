a0b520bc3839ba16a026112811f95ab7681eb1c9
DayYoung <machuanjia@laiye.com>
2021-08-06 16:35:02 +0800

    feat: change

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y
  * @Date: 2021-02-04 10:15:26
- * @LastEditTime: 2021-08-03 11:07:21
+ * @LastEditTime: 2021-08-06 16:34:42
  * @LastEditors: D.Y
  * @FilePath: /pherusa-pro/src/components/index.ts
  * @Description: 组件导出文件
@@ -10,6 +10,7 @@
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as NoFond } from './404';
 export { default as RouteViewer } from './RouteView';
+export { generateRoutes } from './RouteView/RouteGenerator';
 export { default as GlobalLoading } from './GlobalLoading';
 export * from './RouteView/RouteGenerator';
 export { default as ContentLayoutComponent } from './ContentLayout';
@@ -17,4 +18,4 @@ export { listMixin } from './ListMixin';
 export { asyncComponent } from './RouteView/AsyncComponent';
 export { default as PartLoading } from './PartLoading';
 export { default as Icon } from './Icon';
-export { Text } from './Text'
+export { Text } from './Text';
